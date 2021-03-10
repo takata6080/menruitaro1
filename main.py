e696062a25796b671f74a44d3fe60ab6ec4d4770
takata6080 <k.fmb1022.t@gmail.com>
2021-03-11 01:14:54 +0900

    edit1

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def handle_message(event):
         returnMessage = '1'
     else:
         returnMessage = '2'
-    line_bot_api.reply_message(event.reply_token,returnMessage)
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=returnMessage))
 
 
 if __name__ == "__main__":
