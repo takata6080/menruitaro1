776ae470a3f90cf041d921e568515b4f4aba2d2b
takata6080 <k.fmb1022.t@gmail.com>
2021-03-17 00:00:25 +0900

    edit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, abort
+# from config
 # from flask.ext.sqlalchemy import SQLAlchemy
 # from flask import render_template, request
 
@@ -9,11 +10,17 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, ImageSendMessage, VideoSendMessage, StickerSendMessage, AudioSendMessage
+    MessageEvent, TextMessage, TextSendMessage, ImageSendMessage, VideoSendMessage, StickerSendMessage, AudioSendMessage,Event
 )
 import os
 import random
 import sub
+
+# import psycopg2 
+# import psycopg2.extras
+import os
+
+
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
@@ -25,6 +32,17 @@ LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
 
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+# モデル作成
+# class MyCity(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     user_id = db.Column(db.String(80))
+#     hnt_member = db.Column(db.String(80))
+
+#     def __init__(self, user_id, hnt_member):
+#         self.user_id = user_id
+#         self.hnt_member = hnt_member
+
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -48,6 +66,19 @@ def callback():
 def handle_message(event):
     returnMessage = sub.return_message(event.message.text)
     line_bot_api.reply_message(event.reply_token,TextSendMessage(text=returnMessage))
+
+# @handler.add(Event, timestamp=ImageMessage)
+# def handle_image(event):
+
+#     main_image_path = "images/main.jpg"
+#     preview_image_path = "images/preview.jpg"
+
+#     # 画像の送信
+#     image_message = ImageSendMessage(
+#         preview_image_url=f"https://menruitaro1.herokuapp.com/{preview_image_path}"
+#     )
+
+#     line_bot_api.reply_message(event.reply_token, image_message)
 
 # # モデル作成
 # class User(db.Model):
@@ -100,3 +131,39 @@ if __name__ == "__main__":
 #    app.run()
     port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
+    # #postgreSQLに接続（接続情報は環境変数、PG_XXX）
+    # connection = psycopg2.connect(\ 
+    #     host=os.environ.get('PG_HOST'),\ 
+    #     user=os.environ.get('PG_USER'),\ 
+    #     password=os.environ.get('PG_PASS'),\ 
+    #     database=os.environ.get('PG_DBNM'),\ 
+    #     port=int(os.environ.get('PG_PORT'))) 
+
+    # #クライアントプログラムのエンコードを設定（DBの文字コードから自動変換してくれる）
+    # connection.set_client_encoding('utf-8') 
+
+    # #select結果を辞書形式で取得するように設定 
+    # connection.cursor_factory=psycopg2.extras.DictCursor
+
+    # #カーソルの取得
+    # cursor = connection.cursor() 
+
+    # #SQL文設定(%はバインド変数）
+    # sql = 'select name, work from m_user where id = %(target_id)s'
+
+    # #SQL実行
+    # target_id = 1
+    # cursor.execute(sql, {'target_id': (target_id,)}) 
+
+    # #取得結果を出力　★辞書形式　row['name']　辞書形式でなければ、row[0]、orz
+    # results = cursor.fetchall() 
+    # for row in results: 
+    #     print(row['name'])
+    #     print(row['work'])
+
+    # #カーソルをとじる
+    # cursor.close() 
+
+    # #切断
+    # connection.close() 
--- sub.py
+++ sub.py
@@ -1,8 +1,11 @@
+import random
+
 def Numer0n():
     print('ヌメロンを始めます！！')
-    input('3桁の数字を入力してください')
+    my_number = input('3桁の数字を入力してください(同じ数字はNG):')
+    print(my_number)
 
-    return
+    return my_number
 
 
 def return_message(message):
@@ -12,7 +15,63 @@ def return_message(message):
         returnMessage = '今日おすすめの高速道路は...東名高速！！'
     elif message=='今日の麺類' or message=='麺類':
         returnMessage = '今日おすすめの麺類は...日高屋のチゲ味噌ラーメン！！'
+    elif '日向坂' in message:
+        returnMessage = 'メンバーを入力して下さい\n'.join(hnt_list('name'))
+    
     else:
         returnMessage = 'YO!YO!' + message + 'だYo！'
 
     return returnMessage
+
+def hnt_list(select):
+    if select == 'name':
+        hnt_list = [
+            '佐々木久美',
+            '佐々木美鈴',
+            '斎藤京子',
+            '高本彩花',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+        ] 
+    elif select == 'message':
+        hnt_list = [
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+        ]
+    return hnt_list
